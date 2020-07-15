c7aa2cdb4009550e0f6ac61eabf718a3e0c46c0e
Kshitij Sobti <kshitij@sobti.in>
2020-07-15 15:09:48 +0530

    Update to work with latest version

--- test_template_plugin/apps.py
+++ test_template_plugin/apps.py
@@ -18,14 +18,28 @@ class TestTemplatePluginConfig(AppConfig):
     plugin_app = {
         'slots_config': {
             'lms.djangoapp': {
-                'head-extra': 'test_template_plugin.plugin.head_extra_slot',
-                'body-initial': 'test_template_plugin.plugin.body_initial_slot',
-                'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                'instructor_dashboard_2': {
+                    'head-extra': 'test_template_plugin.plugin.head_extra_slot',
+                    'body-initial': 'test_template_plugin.plugin.body_initial_slot',
+                    'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                },
+                'student.views.dashboard': {
+                    'head-extra': 'test_template_plugin.plugin.head_extra_slot',
+                    'body-initial': 'test_template_plugin.plugin.body_initial_slot',
+                    'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                }
             },
             'studio.djangoapp': {
-                'head-extra': 'test_template_plugin.plugin.head_extra_slot',
-                'body-initial': 'test_template_plugin.plugin.body_initial_slot',
-                'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                'course_listing': {
+                    'head-extra': 'test_template_plugin.plugin.head_extra_slot',
+                    'body-initial': 'test_template_plugin.plugin.body_initial_slot',
+                    'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                },
+                'settings_handler': {
+                    'head-extra': 'test_template_plugin.plugin.head_extra_slot',
+                    'body-initial': 'test_template_plugin.plugin.body_initial_slot',
+                    'body-extra': 'test_template_plugin.plugin.body_extra_slot',
+                },
             }
         }
     }
